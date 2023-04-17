f8a9ed2e4f309307d74f6e43b4f173eb031f1c09
a.rogov <a.rogov@tinkoff.ru>
2023-04-18 00:30:44 +0500

    fix

--- chat/message.py
+++ chat/message.py
@@ -2,11 +2,11 @@ from pydantic import BaseModel
 
 
 class Message(BaseModel):
-    chat_id: str = ""
-    timestamp: int = 1
-    sender_id: str = ""
-    body: str = ""
-    gossip_id: str = ""
+    chat_id: str
+    timestamp: int
+    sender_id: str
+    body: str
+    gossip_id: str
 
     class Config:
         frozen=True
